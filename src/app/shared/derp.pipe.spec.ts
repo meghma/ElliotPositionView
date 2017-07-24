dde20f1a448b33263648a10ccb14ac793b58f400
Anand Chaudhuri <Anand Chaudhuri>
2017-07-23 23:23:52 -0400

    delete unused

--- src/app/shared/derp.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DerpPipe } from './derp.pipe';
-
-describe('DerpPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DerpPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
